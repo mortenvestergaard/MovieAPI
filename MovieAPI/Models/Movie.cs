6565710b1ed36056b3b970523b374a7b5c6bc8d8
Morten Vestergaard <mort286f@zbc.dk>
2022-08-16 09:31:42 +0200

    Add project files.

---
+++ MovieAPI/Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿namespace MovieAPI.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string? Poster { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? ReleaseDate { get; set; }
+        public string? Runtime { get; set; }
+
+    }
+}
